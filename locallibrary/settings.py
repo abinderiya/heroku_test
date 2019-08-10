c29140ee45faa805e8ce7376caefdf23d0c3accf
Binderiya Adishaa <b.adishaa@mail.utoronto.ca>
2019-08-09 22:09:18 -0400

    Added allowed host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=yk08k*_e2&#3-cyirbh82h23sh&@bre957!i17nji=^46t5gf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hidden-ravine-98939.herokuapp.com',]
 
 
 # Application definition
